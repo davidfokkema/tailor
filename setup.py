601c2790119ebc823a867810991336629a54b929
David Fokkema <davidfokkema@gmail.com>
2021-02-10 15:52:45 +0100

    Use src/ layout
    As was the default for Beeware but old habits...

--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="analyser",
-    version="0.1",
-    packages=find_packages(),
-    package_data={"analyser": ["analyser.ui", "create_plot_dialog.ui", "plot_tab.ui"]},
-    entry_points={"console_scripts": ["analyser = analyser.main_app:main"]},
-)
