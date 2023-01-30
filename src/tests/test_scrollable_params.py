af5c85c88f2e7a39abcaed1d22aa4c1df22c4aed
David Fokkema <davidfokkema@icloud.com>
2023-01-30 16:14:41 +0100

    Test for a scrollable parameter list

---
+++ src/tests/test_scrollable_params.py
@@ -0,0 +1,30 @@
+from PySide6 import QtWidgets
+
+
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setMinimumSize(500, 300)
+        self.scrollable = QtWidgets.QScrollArea()
+        self.scrollable.setWidgetResizable(True)
+
+        widget = QtWidgets.QWidget()
+        layout = QtWidgets.QVBoxLayout()
+        widget.setLayout(layout)
+
+        self.setCentralWidget(self.scrollable)
+        self.scrollable.setWidget(widget)
+
+        for _ in range(1):
+            layout.addWidget(QtWidgets.QPushButton("OK"))
+
+        for _ in range(1):
+            layout.addWidget(QtWidgets.QPushButton("OK"))
+
+
+if __name__ == "__main__":
+    qapp = QtWidgets.QApplication()
+    app = MainWindow()
+
+    app.show()
+    qapp.exec()
