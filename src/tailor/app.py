22b93f7bb804f5d6254acc82e287dbf71e410259
David Fokkema <davidfokkema@icloud.com>
2023-03-17 16:59:44 +0100

    Moved lots of datasheet code to DataSheet

--- src/tailor/app.py
+++ src/tailor/app.py
@@ -27,7 +27,7 @@ from tailor.csv_format_dialog import (
     NUM_FORMAT_CHOICES,
     CSVFormatDialog,
 )
-from tailor.data_model import MSG_TIMEOUT, DataModel
+from tailor.data_sheet import DataSheet
 from tailor.plot_tab import PlotTab
 from tailor.ui_create_plot_dialog import Ui_CreatePlotDialog
 from tailor.ui_tailor import Ui_MainWindow
@@ -62,6 +62,7 @@ class Application(QtWidgets.QMainWindow):
     _recent_files_actions = None
     _selected_col_idx = None
     _plot_num = 1
+    _sheet_num = 1
 
     _is_dirty = False
 
@@ -85,7 +86,7 @@ class Application(QtWidgets.QMainWindow):
         self._dirty_timer.timeout.connect(self.mark_project_dirty)
 
         # clear all program state
-        # self.clear_all()
+        self.clear_all()
 
         # Enable close buttons...
         self.ui.tabWidget.setTabsClosable(True)
@@ -112,16 +113,17 @@ class Application(QtWidgets.QMainWindow):
             lambda: self.export_graph(".png")
         )
         self.ui.actionClose.triggered.connect(self.new_project)
-        self.ui.actionAdd_column.triggered.connect(self.add_column)
-        self.ui.actionAdd_calculated_column.triggered.connect(
-            self.add_calculated_column
-        )
-        self.ui.actionAdd_row.triggered.connect(self.add_row)
-        self.ui.actionRemove_column.triggered.connect(self.remove_column)
-        self.ui.actionRemove_row.triggered.connect(self.remove_row)
-        self.ui.actionClear_Cell_Contents.triggered.connect(self.clear_selected_cells)
-        self.ui.actionCopy.triggered.connect(self.copy_selected_cells)
-        self.ui.actionPaste.triggered.connect(self.paste_cells)
+        # WIP
+        # self.ui.actionAdd_column.triggered.connect(self.add_column)
+        # self.ui.actionAdd_calculated_column.triggered.connect(
+        #     self.add_calculated_column
+        # )
+        # self.ui.actionAdd_row.triggered.connect(self.add_row)
+        # self.ui.actionRemove_column.triggered.connect(self.remove_column)
+        # self.ui.actionRemove_row.triggered.connect(self.remove_row)
+        # self.ui.actionClear_Cell_Contents.triggered.connect(self.clear_selected_cells)
+        # self.ui.actionCopy.triggered.connect(self.copy_selected_cells)
+        # self.ui.actionPaste.triggered.connect(self.paste_cells)
 
         # set up the open recent menu
         self.ui._recent_files_separator = self.ui.menuOpen_Recent.insertSeparator(
@@ -170,30 +172,6 @@ class Application(QtWidgets.QMainWindow):
         #         QtGui.QKeySequence(key), self.ui.data_view, self.clear_selected_cells
         #     )
 
-    def _set_view_and_selection_model(self):
-        """Set up data view and selection model.
-
-        Connects the table widget to the data model, sets up various behaviours
-        and resets visual column ordering.
-        """
-        self.ui.data_view.setModel(self.data_model)
-        self.ui.data_view.setDragDropMode(self.ui.data_view.NoDragDrop)
-        header = self.ui.data_view.horizontalHeader()
-        header.setSectionsMovable(True)
-        header.setSectionResizeMode(QtWidgets.QHeaderView.ResizeToContents)
-        header.setMinimumSectionSize(header.defaultSectionSize())
-
-        # reset column ordering. There is, apparently, no easy way to do this :'(
-        for log_idx in range(self.data_model.columnCount()):
-            # move sections in the correct position FROM LEFT TO RIGHT
-            # so, logical indexes should be numbered [0, 1, 2, ... ]
-            # >>> header.moveSection(from, to)
-            vis_idx = header.visualIndex(log_idx)
-            header.moveSection(vis_idx, log_idx)
-
-        self.selection = self.ui.data_view.selectionModel()
-        self.selection.selectionChanged.connect(self.selection_changed)
-
     def mark_project_dirty(self, is_dirty=True):
         """Mark project as dirty"""
         self._is_dirty = is_dirty
@@ -201,35 +179,6 @@ class Application(QtWidgets.QMainWindow):
         if not is_dirty:
             # FIXME: this can be implemented much better by actually detecting changes.
             self._dirty_timer.start(DIRTY_TIMEOUT)
-
-    def column_moved(self, logidx, oldidx, newidx):
-        """Move column in reaction to UI signal.
-
-        Dragging a column to a new location triggers execution of this method.
-        Since the UI only reorders the column visually and does not change the
-        underlying data, things can get tricky when trying to determine which
-        variables are available to the left of calculated columns and which
-        columns include the bouding box of a selection for copy/paste. So, we
-        will immediately move back the column in the view and move the
-        underlying data instead. That way, visual and logical ordering are
-        always in sync.
-
-        Args:
-            logidx (int): the logical column index (index in the dataframe)
-            oldidx (int): the old visual index newidx (int): the new visual
-            index
-        """
-        print(f"Column moved from {oldidx=} to {newidx=}")
-        header = self.ui.data_view.horizontalHeader()
-        header.blockSignals(True)
-        # move the column back, keep the header in logical order
-        header.moveSection(newidx, oldidx)
-        header.blockSignals(False)
-        # move the underlying data column instead
-        self.data_model.moveColumn(None, oldidx, None, newidx)
-        self.data_model.recalculate_all_columns()
-        # select the column that was just moved at the new location
-        self.ui.data_view.selectColumn(newidx)
 
     def eventFilter(self, watched, event):
         """Catch PySide6 events.
@@ -275,168 +224,6 @@ class Application(QtWidgets.QMainWindow):
         )
         box.exec()
 
-    def edit_or_move_down(self):
-        """Edit cell or move cursor down a row.
-
-        Start editing a cell. If the cell was already being edited, move the
-        cursor down a row, stopping the edit in the process. Trigger a
-        recalculation of all calculated columns.
-        """
-        cur_index = self.ui.data_view.currentIndex()
-        if not self.ui.data_view.isPersistentEditorOpen(cur_index):
-            # is not yet editing, so start an edit
-            self.ui.data_view.edit(cur_index)
-        else:
-            # is already editing, what index is below?
-            new_index = self.get_index_below_selected_cell()
-            if new_index == cur_index:
-                # already on bottom row, create a new row and take that index
-                self.add_row()
-                new_index = self.get_index_below_selected_cell()
-            # move to it (finishing editing in the process)
-            self.ui.data_view.setCurrentIndex(new_index)
-
-    def clear_selected_cells(self):
-        """Clear contents of selected cells."""
-        for index in self.selection.selectedIndexes():
-            self.data_model.setData(index, "", skip_update=True)
-        # signal that ALL values may have changed using an invalid index
-        # this can be MUCH quicker than emitting signals for each cell
-        self.data_model.dataChanged.emit(QtCore.QModelIndex(), QtCore.QModelIndex())
-        # recalculate computed values once
-        self.data_model.recalculate_all_columns()
-
-    def get_index_below_selected_cell(self):
-        """Get index directly below the selected cell."""
-        return self.ui.data_view.moveCursor(
-            self.ui.data_view.MoveDown, QtCore.Qt.NoModifier
-        )
-
-    def copy_selected_cells(self):
-        """Copy selected cells to clipboard."""
-
-        # get bounding rectangle coordinates and sizes
-        selection = self.selection.selection().toList()
-        left = min(r.left() for r in selection)
-        width = max(r.right() for r in selection) - left + 1
-        top = min(r.top() for r in selection)
-        height = max(r.bottom() for r in selection) - top + 1
-
-        # fill data from selected indexes, not selected -> NaN
-        data = np.full((height, width), np.nan)
-        for index in self.selection.selectedIndexes():
-            if (value := index.data()) == "":
-                value = np.nan
-            data[index.row() - top, index.column() - left] = value
-
-        # create tab separated values from data, NaN -> empty string, e.g.
-        # 1 2 3
-        # 2   4
-        # 5 5 6
-        text = "\n".join(
-            [
-                "\t".join([str(v) if not np.isnan(v) else "" for v in row])
-                for row in data
-            ]
-        )
-
-        # write TSV text to clipboard
-        self.clipboard.setText(text)
-
-    def paste_cells(self):
-        """Paste cells from clipboard."""
-
-        # get data from clipboard
-        text = self.clipboard.text()
-
-        # create array from tab separated values, "" -> NaN
-        try:
-            data = np.array(
-                [
-                    [float(v) if v != "" else np.nan for v in row.split("\t")]
-                    for row in text.split("\n")
-                ]
-            )
-        except ValueError as exc:
-            self.ui.statusbar.showMessage(
-                f"Error pasting from clipboard: {exc}", timeout=MSG_TIMEOUT
-            )
-            return
-
-        # get current coordinates and data size
-        current_index = self.ui.data_view.currentIndex()
-        start_row, start_column = current_index.row(), current_index.column()
-        height, width = data.shape
-
-        # extend rows and columns if necessary
-        last_table_column = self.data_model.columnCount() - 1
-        if (last_data_column := start_column + width - 1) > last_table_column:
-            for _ in range(last_data_column - last_table_column):
-                self.add_column()
-        last_table_row = self.data_model.rowCount() - 1
-        if (last_data_row := start_row + height - 1) > last_table_row:
-            for _ in range(last_data_row - last_table_row):
-                self.add_row()
-
-        # write clipboard data to data model
-        it = np.nditer(data, flags=["multi_index"])
-        for value in it:
-            row, column = it.multi_index
-            self.data_model.setData(
-                self.data_model.createIndex(row + start_row, column + start_column),
-                value,
-                skip_update=True,
-            )
-
-        # signal that values have changed
-        self.data_model.dataChanged.emit(
-            self.data_model.createIndex(start_row, start_column),
-            self.data_model.createIndex(
-                start_row + height - 1, start_column + width - 1
-            ),
-        )
-        # recalculate computed values once
-        self.data_model.recalculate_all_columns()
-        # reset current index and focus
-        self.ui.data_view.setCurrentIndex(current_index)
-        self.ui.data_view.setFocus()
-
-    def selection_changed(self, selected, deselected):
-        """Handle selectionChanged events in the data view.
-
-        When the selection is changed, the column index of the left-most cell in
-        the first selection is used to identify the column name and the
-        mathematical expression that is used to calculate the column values.
-        These values are used to update the column information in the user
-        interface.
-
-        Args:
-            selected: QItemSelection containing the newly selected events.
-            deselected: QItemSelection containing previously selected, and now
-            deselected, items.
-        """
-        if not selected.isEmpty():
-            self.ui.nameLabel.setEnabled(True)
-            self.ui.name_edit.setEnabled(True)
-            first_selection = selected.first()
-            col_idx = first_selection.left()
-            self._selected_col_idx = col_idx
-            self.ui.name_edit.setText(self.data_model.get_column_name(col_idx))
-            self.ui.formula_edit.setText(self.data_model.get_column_expression(col_idx))
-            if self.data_model.is_calculated_column(col_idx):
-                self.ui.formulaLabel.setEnabled(True)
-                self.ui.formula_edit.setEnabled(True)
-            else:
-                self.ui.formulaLabel.setEnabled(False)
-                self.ui.formula_edit.setEnabled(False)
-        else:
-            self.ui.nameLabel.setEnabled(False)
-            self.ui.name_edit.clear()
-            self.ui.name_edit.setEnabled(False)
-            self.ui.formulaLabel.setEnabled(False)
-            self.ui.formula_edit.clear()
-            self.ui.formula_edit.setEnabled(False)
-
     def tab_changed(self, idx):
         """Handle currentChanged events of the tab widget.
 
@@ -470,73 +257,6 @@ class Application(QtWidgets.QMainWindow):
         for idx in range(self.ui.tabWidget.count()):
             self.update_plot_tab(idx)
 
-    def add_column(self):
-        """Add column to data model and select it."""
-        col_index = self.data_model.columnCount()
-        self.data_model.insertColumn(col_index)
-        self.ui.data_view.selectColumn(col_index)
-        self.ui.name_edit.selectAll()
-        self.ui.name_edit.setFocus()
-
-    def add_calculated_column(self):
-        """Add a calculated column to data model and select it."""
-        col_index = self.data_model.columnCount()
-        self.data_model.insert_calculated_column(col_index)
-        self.ui.data_view.selectColumn(col_index)
-        self.ui.name_edit.selectAll()
-        self.ui.name_edit.setFocus()
-
-    def add_row(self):
-        """Add row to data model."""
-        self.data_model.insertRow(self.data_model.rowCount())
-
-    def remove_column(self):
-        """Remove selected column(s) from data model."""
-        selected_columns = [s.column() for s in self.selection.selectedColumns()]
-        if selected_columns:
-            # Remove columns in reverse order to avoid index shifting during
-            # removal. WIP: It would be more efficient to merge ranges of
-            # contiguous columns since they can be removed in one fell swoop.
-            selected_columns.sort(reverse=True)
-            for column in selected_columns:
-                self.data_model.removeColumn(column)
-        else:
-            error_msg = QtWidgets.QMessageBox()
-            error_msg.setText("You must select one or more columns.")
-            error_msg.exec()
-
-    def remove_row(self):
-        """Remove selected row(s) from data model."""
-        selected_rows = [s.row() for s in self.selection.selectedRows()]
-        if selected_rows:
-            # Remove rows in reverse order to avoid index shifting during
-            # removal. WIP: It would be more efficient to merge ranges of
-            # contiguous rows since they can be removed in one fell swoop.
-            selected_rows.sort(reverse=True)
-            for row in selected_rows:
-                self.data_model.removeRow(row)
-        else:
-            error_msg = QtWidgets.QMessageBox()
-            error_msg.setText("You must select one or more rows.")
-            error_msg.exec()
-
-    def rename_column(self, name):
-        """Rename a column.
-
-        Renames the currently selected column.
-
-        Args:
-            name: a QString containing the new name.
-        """
-        if self._selected_col_idx is not None:
-            # Do not allow empty names or duplicate column names
-            if name and name not in self.data_model.get_column_names():
-                old_name = self.data_model.get_column_name(self._selected_col_idx)
-                new_name = self.data_model.rename_column(self._selected_col_idx, name)
-                self.rename_plot_variables(old_name, new_name)
-                # set the normalized name to the name edit field
-                self.ui.name_edit.setText(new_name)
-
     def rename_plot_variables(self, old_name, new_name):
         """Rename any plotted variables
 
@@ -567,18 +287,6 @@ class Application(QtWidgets.QMainWindow):
                             tab.update_function_label(new_name)
                 if needs_info_update:
                     tab.update_info_box()
-
-    def update_column_expression(self, expression):
-        """Update a column expression.
-
-        Tries to recalculate the values of the currently selected column in the
-        data model.
-
-        Args:
-            expression: a QString containing the mathematical expression.
-        """
-        if self._selected_col_idx is not None:
-            self.data_model.update_column_expression(self._selected_col_idx, expression)
 
     def ask_and_create_plot_tab(self):
         """Opens a dialog and create a new tab with a plot.
@@ -649,15 +357,16 @@ class Application(QtWidgets.QMainWindow):
 
         Closes all tabs and data.
         """
-        for idx in range(self.ui.tabWidget.count(), 0, -1):
-            # close all plot tabs in reverse order, they are no longer valid
-            self.ui.tabWidget.removeTab(idx)
+        self.ui.tabWidget.clear()
         self._plot_num = 1
-        self.data_model = DataModel(main_app=self)
-        self._set_view_and_selection_model()
-        self.ui.data_view.setCurrentIndex(self.data_model.createIndex(0, 0))
+        self._sheet_num = 1
+        self.add_data_sheet()
         self._set_project_path(None)
         self.mark_project_dirty(False)
+
+    def add_data_sheet(self):
+        self.ui.tabWidget.addTab(DataSheet(self), f"Sheet{self._sheet_num}")
+        self._sheet_num += 1
 
     def new_project(self):
         """Close the current project and open a new one."""
