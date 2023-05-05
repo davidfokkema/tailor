72cf45f66e1b49a3a057a4baf8b09b325e5ad4c3
David Fokkema <davidfokkema@icloud.com>
2023-05-05 17:48:35 +0200

    Update test_simple_references.py

--- src/tests/test_simple_references.py
+++ src/tests/test_simple_references.py
@@ -5,7 +5,7 @@ sys.path.append("src/")
 
 import numpy as np
 import pandas as pd
-from PySide6 import QtCore, QtWidgets
+from PySide6 import QtWidgets
 
 from tailor.app import Application
 
@@ -15,7 +15,13 @@ def test_move_column(app: Application):
     sheet1 = app.ui.tabWidget.currentWidget()
     sheet1.data_model.beginResetModel()
     sheet1.data_model._data = pd.DataFrame.from_dict(
-        {"x": x, "y": x**2, "z": x / 2, "a": x * 2, "b": np.sin(x)}
+        {
+            "x": x,
+            "y": x**2 + np.random.normal(scale=10, size=x.shape),
+            "z": x / 2,
+            "a": x * 2,
+            "b": np.sin(x),
+        }
     )
     sheet1.data_model.endResetModel()
     sheet1.add_calculated_column()
