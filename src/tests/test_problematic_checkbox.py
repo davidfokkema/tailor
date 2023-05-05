5c5e53f2600338338333e8fe855801d38f58f87e
David Fokkema <davidfokkema@icloud.com>
2023-05-05 17:24:37 +0200

    Test case: fit problem was checkState()

---
+++ src/tests/test_problematic_checkbox.py
@@ -0,0 +1,10 @@
+from PySide6 import QtWidgets
+
+app = QtWidgets.QApplication()
+checkbox = QtWidgets.QCheckBox()
+state = checkbox.checkState()
+print(f"{state=}")
+# this is True on PySide 6.3 but False on PySide 6.4 !!
+print(f"{not state=}")
+
+print(f"{not checkbox.isChecked()=}")
