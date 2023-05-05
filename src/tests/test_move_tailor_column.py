18efee8b302914fc49b0848665f4b2f8fbb842a5
David Fokkema <davidfokkema@icloud.com>
2023-05-05 15:29:34 +0200

    Update test_move_tailor_column.py

--- src/tests/test_move_tailor_column.py
+++ src/tests/test_move_tailor_column.py
@@ -12,18 +12,18 @@ from tailor.app import Application
 
 def test_move_column(app: Application):
     x = np.arange(20)
-    app.data_model.beginResetModel()
-    app.data_model._data = pd.DataFrame.from_dict(
+    sheet1 = app.ui.tabWidget.currentWidget()
+    sheet1.data_model.beginResetModel()
+    sheet1.data_model._data = pd.DataFrame.from_dict(
         {"x": x, "y": x**2, "z": x / 2, "a": x * 2, "b": np.sin(x)}
     )
-    app.data_model.endResetModel()
-    app.add_calculated_column()
-    app.ui.name_edit.setText("func1")
-    app.rename_column("func1")
-    app.ui.formula_edit.setText("y ** 2")
-    app.update_column_expression("y ** 2")
+    sheet1.data_model.endResetModel()
+    sheet1.add_calculated_column()
+    sheet1.rename_column("func1")
+    sheet1.ui.formula_edit.setText("y ** 2")
+    sheet1.update_column_expression("y ** 2")
 
-    app.ui.data_view.horizontalHeader().moveSection(1, 3)
+    sheet1.ui.data_view.horizontalHeader().moveSection(1, 3)
 
 
 if __name__ == "__main__":
