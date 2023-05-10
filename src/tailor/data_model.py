68538df6266cad98fe455e6d884406c189047955
David Fokkema <davidfokkema@icloud.com>
2023-05-10 20:33:48 +0200

    Split up data model in GUI / logic parts

--- src/tailor/data_model.py
+++ src/tailor/data_model.py
@@ -1,344 +1,31 @@
 """Data model for the tailor app.
 
-Implements a QAbstractDataModel to contain the data values as a backend for the
-table view used in the app.
+Implements a model to contain the data values as a backend for the
+table view used in the app. This class provides an API specific to Tailor.
 """
-import re
 
-import asteval
-import numpy as np
 import pandas as pd
-from PySide6 import QtCore, QtGui
 
 # treat Inf and -Inf as missing values (e.g. when calling dropna())
 pd.options.mode.use_inf_as_na = True
 
 
-MSG_TIMEOUT = 0
-
-
-class DataModel(QtCore.QAbstractTableModel):
+class DataModel:
     """Data model for the tailor app.
 
-    Implements a QAbstractDataModel to contain the data values as a backend for the
-    table view used in the app.
+    Implements a model to contain the data values as a backend for the
+    table view used in the app. This class provides an API specific to Tailor.
     """
 
-    _new_col_num = 0
-    _data = None
-    _calculated_column_expression = None
-    _is_calculated_column_valid = None
-
-    def __init__(self):
-        """Instantiate the class."""
-        super().__init__()
-
-        # self._data = pd.DataFrame.from_dict({"x": 5 * [np.nan], "y": 5 * [np.nan]})
+    def __init__(self) -> None:
         self._data = pd.DataFrame()
         self._calculated_column_expression = {}
         self._is_calculated_column_valid = {}
-
-    def rowCount(self, parent: QtCore.QModelIndex = QtCore.QModelIndex()) -> int:
-        """Return the number of rows in the data.
-
-        Since a table has no hierarchy, when the parent is valid (i.e. points to
-        an item in the table) this method returns 0 since an item in the table
-        has no rows of itself. If the parent is invalid, return the number of
-        rows in the table.
-
-        Args:
-            parent: the parent item for which to count rows [invalid]
-        """
-        if parent.isValid():
-            return 0
-        else:
-            return len(self._data)
-
-    def columnCount(self, parent: QtCore.QModelIndex = QtCore.QModelIndex()) -> int:
-        """Return the number of columns in the data.
-
-        Since a table has no hierarchy, when the parent is valid (i.e. points to
-        an item in the table) this method returns 0 since an item in the table
-        has no columns of itself. If the parent is invalid, return the number of
-        columns in the table.
-
-        Args:
-            parent: the parent item for which to count columns [invalid]
-        """
-        if parent.isValid():
-            return 0
-        else:
-            return len(self._data.columns)
-
-    def data(
-        self,
-        index: QtCore.QModelIndex,
-        role: QtCore.Qt.ItemDataRole = QtCore.Qt.DisplayRole,
-    ) -> str | QtGui.QBrush:
-        """Return (attributes of) the data.
-
-        This method is called by the table view to request data points or its
-        attributes. For each table cell multiple calls are made, one for each
-        'role'. The role indicates the type of data that is requested. For
-        example, the DisplayRole indicates a string is requested to display in
-        the cell. Also, a BackgroundRole indicates that the background color for
-        the cell is requested. You can use this to indicate different types of
-        data, e.g. calculated or input data.
-
-        If a role is requested that is not implemented an invalid QVariant
-        (None) is returned.
-
-        Args:
-            index: a reference to the requested data item.
-            role: an indication what type of information is
-                requested.
-
-        Returns: The requested data or None.
-        """
-        row = index.row()
-        col = index.column()
-
-        if role in [QtCore.Qt.DisplayRole, QtCore.Qt.EditRole]:
-            # request for the data itself
-            value = self._data.iat[row, col]
-            if np.isnan(value) and not self.is_calculated_column(col):
-                # NaN in a data column, show as empty
-                return ""
-            else:
-                return f"{value:.10g}"
-        elif role == QtCore.Qt.BackgroundRole:
-            # request for the background fill of the cell
-            if self.is_calculated_column(col):
-                if self.is_calculated_column_valid(col):
-                    # Yellow
-                    return QtGui.QBrush(QtGui.QColor(255, 255, 200))
-                else:
-                    # Red
-                    return QtGui.QBrush(QtGui.QColor(255, 200, 200))
-        # not implemented, return an invalid QVariant (None) per the docs
-        # See Qt for Python docs -> Considerations -> API Changes
-        return None
-
-    def setData(
-        self,
-        index: QtCore.QModelIndex,
-        value: float,
-        role: QtCore.Qt.ItemDataRole = QtCore.Qt.EditRole,
-        *,
-        skip_update=False,
-    ) -> bool:
-        """Set (attributes of) data values.
-
-        This method is used to set data values in the model. The role indicates
-        the type of data to set. Currently, only the EditRole is supported for
-        setting values through the table view's cell editing machinery.
-
-        Args:
-            index: a QModelIndex referencing the requested data item.
-            value: the value to set.
-            role: an ItemDataRole to indicate what type of information is
-                requested.
-            skip_update (boolean): if True, do not recalculate computed
-                cell values or signal that the current cell has changed. These
-                are both time-consuming operations.
-
-        Returns:
-            True if the data could be set. False otherwise.
-        """
-        if role == QtCore.Qt.EditRole:
-            row = index.row()
-            col = index.column()
-            try:
-                self._data.iat[row, col] = float(value)
-            except ValueError:
-                self._data.iat[row, col] = np.nan
-            finally:
-                # FIXME: data changed, recalculate all columns; better to only
-                # recalculate the current row
-                if not skip_update:
-                    self.recalculate_all_columns()
-                    self.dataChanged.emit(index, index)
-            return True
-        # Role not implemented
-        return False
-
-    def headerData(
-        self,
-        section: int,
-        orientation: QtCore.Qt.Orientation,
-        role: QtCore.Qt.ItemDataRole = QtCore.Qt.DisplayRole,
-    ):
-        """Return row and column information.
-
-        Return data on the row and column headers. Mainly useful for displaying
-        the names of the columns and the row numbers in the table view. The
-        orientation should be specified as Qt.Horizontal for column headers and
-        Qt.Vertical for row headers. The role should specify what type of data is requested.
-
-        Args:
-            section: an integer section (row or column) number.
-            orientation: the orientation of the header.
-            role: an ItemDataRole to indicate what type of information is
-                requested.
-
-        Returns:
-            The data, if available, or an invalid QVariant (None).
-        """
-        if role == QtCore.Qt.DisplayRole:
-            if orientation == QtCore.Qt.Horizontal:
-                return self._data.columns[section]
-            else:
-                return str(self._data.index[section] + 1)
-        # See Qt for Python docs -> Considerations -> API Changes
-        return None
 
     def is_empty(self):
         """Check whether all cells are empty."""
         # check for *all* nans in a row or column
         return self._data.dropna(how="all").empty
-
-    def moveColumn(
-        self, sourceParent, sourceColumn, destinationParent, destinationChild
-    ):
-        """Move column.
-
-        Move a column from sourceColumn to destinationChild. Alas, the Qt naming
-        scheme remains a bit of a mystery.
-
-        Args:
-            sourceParent: ignored.
-            sourceColumn (int): the source column number.
-            destinationParent: ignored.
-            destinationChild (int): the destination column number.
-
-        Returns:
-            bool: True if the column was moved.
-        """
-        cols = list(self._data.columns)
-        cols.insert(destinationChild, cols.pop(sourceColumn))
-        self._data = self._data[cols]
-        return True
-
-    def insertColumns(
-        self, column: int, count: int, parent: QtCore.QModelIndex = QtCore.QModelIndex()
-    ) -> bool:
-        """Insert columns into the table.
-
-        Insert columns *before* the specified column number. Returns True if
-        the insertion was succesful.
-
-        Args:
-            column: a column number to indicate the place of insertion.
-            count: the number of columns to insert.
-            parent: a QModelIndex pointing to the model. Must be invalid since
-                you can't insert columns into a cell.
-
-        Returns:
-            True if the insertion was succesful, False otherwise.
-        """
-        if parent.isValid():
-            # a table cell can _not_ insert columns
-            return False
-
-        labels = [self._create_new_column_label() for _ in range(count)]
-
-        self.beginInsertColumns(QtCore.QModelIndex(), column, column)
-        for idx, label in zip(range(column, column + count), labels):
-            self._data.insert(idx, label, np.nan)
-        self.endInsertColumns()
-        return True
-
-    def removeColumns(
-        self, column: int, count: int, parent: QtCore.QModelIndex = QtCore.QModelIndex()
-    ) -> bool:
-        """Remove columns from the table.
-
-        Removes a column at the specified column number. Returns True if the
-        removal was succesful.
-
-        Args:
-            column: a column number to indicate the place of removal.
-            count: the number of columns to remove.
-            parent: a QModelIndex pointing to the model. Must be invalid since
-                you can't remove columns from a cell.
-
-        Returns:
-            True if the removal was succesful, False otherwise.
-        """
-        if parent.isValid():
-            # a table cell can _not_ remove columns
-            return False
-
-        labels = self._data.columns[column : column + count]
-        self.beginRemoveColumns(QtCore.QModelIndex(), column, column)
-        self._data.drop(columns=labels, inplace=True)
-        # try:
-        #     del self._calculated_column_expression[column_name]
-        # except KeyError:
-        #     # not a calculated column
-        #     pass
-        self.endRemoveColumns()
-        # self.recalculate_all_columns()
-        return True
-
-    def insertRows(
-        self, row: int, count: int, parent: QtCore.QModelIndex = QtCore.QModelIndex()
-    ) -> bool:
-        """Insert rows into the table.
-
-        Insert `count` rows into the table at position `row`. Returns True if
-        the insertion was succesful.
-
-        Args:
-            row: an integer row number to indicate the place of insertion.
-            count: number of rows to insert
-            parent: a QModelIndex pointing into the model. Must be invalid since
-                you cannot insert rows into a cell.
-
-        Returns:
-            True if the insertion was succesful, False otherwise.
-        """
-        if parent.isValid():
-            # a table cell can _not_ insert rows
-            return False
-
-        self.beginInsertRows(parent, row, row + count)
-        new_data = pd.DataFrame.from_dict(
-            {col: count * [np.nan] for col in self._data.columns}
-        )
-        self._data = pd.concat(
-            [self._data.iloc[:row], new_data, self._data.iloc[row:]]
-        ).reset_index(drop=True)
-        self.endInsertRows()
-        self.recalculate_all_columns()
-        return True
-
-    def removeRows(
-        self, row: int, count: int, parent: QtCore.QModelIndex = QtCore.QModelIndex()
-    ) -> bool:
-        """Remove rows from the table.
-
-        Removes a row at the specified row number. Returns True if the
-        removal was succesful.
-
-        Args:
-            row: an integer row number to indicate the place of removal.
-            parent: a QModelIndex pointing to the model (ignored).
-
-        Returns:
-            True if the removal was succesful, False otherwise.
-        """
-        if parent.isValid():
-            # a table cell can _not_ remove rows
-            return False
-
-        index = self._data.index[row : row + count]
-        self.beginRemoveRows(QtCore.QModelIndex(), row, row)
-        self._data.drop(index, axis=0, inplace=True)
-        self._data.reset_index(drop=True, inplace=True)
-        self.endRemoveRows()
-        self.recalculate_all_columns()
-        return True
 
     def insert_calculated_column(self, column):
         """Insert a calculated column.
@@ -456,26 +143,6 @@ class DataModel(QtCore.QAbstractTableModel):
             self.show_status(f"Recalculated column values.")
             return True
 
-    def show_status(self, msg):
-        """Show message in statusbar.
-
-        Args:
-            msg (str): the error message.
-        """
-        self.main_window.ui.statusbar.showMessage(msg, timeout=MSG_TIMEOUT)
-
-    def emit_column_changed(self, col_name):
-        """Emit dataChanged signal for a given column.
-
-        Args:
-            col_name (str): name of the column that contains updated values.
-        """
-        col = self._data.columns.get_loc(col_name)
-        n_rows = len(self._data)
-        begin = self.createIndex(0, col)
-        end = self.createIndex(n_rows - 1, col)
-        self.dataChanged.emit(begin, end)
-
     def _get_accessible_columns(self, col_name):
         """Get accessible column data for use in expressions.
 
@@ -509,24 +176,6 @@ class DataModel(QtCore.QAbstractTableModel):
         for col_idx in range(self.columnCount()):
             if self.is_calculated_column(col_idx):
                 self.recalculate_column(column_names[col_idx])
-
-    def flags(self, index: QtCore.QModelIndex) -> QtCore.Qt.ItemFlag:
-        """Returns item flags.
-
-        Returns the item flags for the given index.
-
-        Args:
-            index: a QModelIndex referencing the requested data item.
-
-        Returns:
-            The requested flags.
-        """
-        flags = QtCore.Qt.ItemIsEnabled | QtCore.Qt.ItemIsSelectable
-        col = index.column()
-        if not self.is_calculated_column(col):
-            # You can only edit data if the column values are not calculated
-            flags |= QtCore.Qt.ItemIsEditable
-        return flags
 
     def get_column_name(self, col_idx):
         """Get column name.
--- tests/test_data_model.py
+++ tests/test_data_model.py
@@ -3,19 +3,19 @@ import pandas as pd
 import pytest
 from PySide6 import QtCore
 
-from tailor.data_model import DataModel
+from tailor.qdata_model import QDataModel
 
 
 @pytest.fixture()
 def model():
-    yield DataModel()
+    yield QDataModel()
 
 
 @pytest.fixture()
-def bare_bones_data(model: DataModel):
+def bare_bones_data(model: QDataModel):
     """Create a bare bones data model.
 
-    This is an instance of DataModel with a very basic data structure (five
+    This is an instance of QDataModel with a very basic data structure (five
     rows, two columns) and an updated column number variable, but nothing else.
     You can use this to test basic data manipulation required by Qt for
     subclasses of QAbstractDataModel.
@@ -34,34 +34,34 @@ def bare_bones_data(model: DataModel):
 
 
 class TestImplementationDetails:
-    def test_model_attributes(self, model: DataModel):
+    def test_model_attributes(self, model: QDataModel):
         assert type(model._data) == pd.DataFrame
         assert model._new_col_num == 0
 
-    def test_new_column_label(self, model: DataModel):
+    def test_new_column_label(self, model: QDataModel):
         labels = [model._create_new_column_label() for _ in range(3)]
         assert labels == ["col1", "col2", "col3"]
         assert model._new_col_num == 3
 
 
 class TestQtRequired:
-    def test_rowCount_row_count(self, bare_bones_data: DataModel):
+    def test_rowCount_row_count(self, bare_bones_data: QDataModel):
         assert bare_bones_data.rowCount() == 5
 
-    def test_rowCount_valid_parent(self, bare_bones_data: DataModel):
+    def test_rowCount_valid_parent(self, bare_bones_data: QDataModel):
         """Valid parent has no children in a table."""
         index = bare_bones_data.createIndex(0, 0)
         assert bare_bones_data.rowCount(index) == 0
 
-    def test_columnCount(self, bare_bones_data: DataModel):
+    def test_columnCount(self, bare_bones_data: QDataModel):
         assert bare_bones_data.columnCount() == 3
 
-    def test_columnCount_valid_parent(self, bare_bones_data: DataModel):
+    def test_columnCount_valid_parent(self, bare_bones_data: QDataModel):
         """Valid parent has no children in a table."""
         index = bare_bones_data.createIndex(0, 0)
         assert bare_bones_data.columnCount(index) == 0
 
-    def test_data_returns_data(self, bare_bones_data: DataModel):
+    def test_data_returns_data(self, bare_bones_data: QDataModel):
         index1 = bare_bones_data.createIndex(2, 1)
         index2 = bare_bones_data.createIndex(3, 0)
 
@@ -73,12 +73,12 @@ class TestQtRequired:
         assert value1 == "8"
         assert value2 == "4"
 
-    def test_data_returns_None_for_invalid_role(self, bare_bones_data: DataModel):
+    def test_data_returns_None_for_invalid_role(self, bare_bones_data: QDataModel):
         index = bare_bones_data.createIndex(2, 1)
         value = bare_bones_data.data(index, QtCore.Qt.DecorationRole)
         assert value is None
 
-    def test_headerData(self, bare_bones_data: DataModel):
+    def test_headerData(self, bare_bones_data: QDataModel):
         assert bare_bones_data.headerData(0, QtCore.Qt.Horizontal) == "col0"
         assert (
             bare_bones_data.headerData(1, QtCore.Qt.Horizontal, QtCore.Qt.DisplayRole)
@@ -92,7 +92,7 @@ class TestQtRequired:
         )
         assert bare_bones_data.headerData(3, QtCore.Qt.Vertical) == "4"
 
-    def test_setData(self, bare_bones_data: DataModel):
+    def test_setData(self, bare_bones_data: QDataModel):
         # WIP: test that this method emits dataChanged
         index1 = bare_bones_data.createIndex(2, 1)
         index2 = bare_bones_data.createIndex(3, 0)
@@ -106,7 +106,7 @@ class TestQtRequired:
         assert bare_bones_data._data.at[2, "col1"] == 1.7
         assert bare_bones_data._data.at[3, "col0"] == 4.2
 
-    def test_flags(self, bare_bones_data: DataModel):
+    def test_flags(self, bare_bones_data: QDataModel):
         index = bare_bones_data.createIndex(2, 1)
         flags = bare_bones_data.flags(index)
         assert (
@@ -116,7 +116,7 @@ class TestQtRequired:
             | QtCore.Qt.ItemIsEditable
         )
 
-    def test_insertRows(self, bare_bones_data: DataModel):
+    def test_insertRows(self, bare_bones_data: QDataModel):
         # WIP: test that begin/endInsertRows is called
         retvalue1 = bare_bones_data.insertRows(3, 4, parent=QtCore.QModelIndex())
         assert retvalue1 is True
@@ -138,28 +138,28 @@ class TestQtRequired:
             nan_ok=True,
         )
 
-    def test_insertRows_valid_parent(self, bare_bones_data: DataModel):
+    def test_insertRows_valid_parent(self, bare_bones_data: QDataModel):
         """You can't add rows inside cells."""
         assert (
             bare_bones_data.insertRows(0, 2, parent=bare_bones_data.createIndex(0, 0))
             is False
         )
 
-    def test_removeRows(self, bare_bones_data: DataModel):
+    def test_removeRows(self, bare_bones_data: QDataModel):
         # WIP: test that begin/endRemoveRows is called
         retvalue = bare_bones_data.removeRows(1, 2)
         assert retvalue is True
         assert list(bare_bones_data._data["col0"]) == pytest.approx([1.0, 4.0, 5.0])
         assert list(bare_bones_data._data["col1"]) == pytest.approx([6.0, 9.0, 10.0])
 
-    def test_removeRows_valid_parent(self, bare_bones_data: DataModel):
+    def test_removeRows_valid_parent(self, bare_bones_data: QDataModel):
         """You can't remove rows inside cells."""
         assert (
             bare_bones_data.removeRows(0, 2, parent=bare_bones_data.createIndex(0, 0))
             is False
         )
 
-    def test_insertColumns(self, bare_bones_data: DataModel):
+    def test_insertColumns(self, bare_bones_data: QDataModel):
         retvalue = bare_bones_data.insertColumns(1, 2)
         assert retvalue is True
         assert bare_bones_data._data.shape == (5, 5)
@@ -167,7 +167,7 @@ class TestQtRequired:
             [1.0, np.nan, np.nan, 6.0, 11.0], nan_ok=True
         )
 
-    def test_insertColumns_valid_parent(self, bare_bones_data: DataModel):
+    def test_insertColumns_valid_parent(self, bare_bones_data: QDataModel):
         """You can't add columns inside cells."""
         assert (
             bare_bones_data.insertColumns(
@@ -176,13 +176,13 @@ class TestQtRequired:
             is False
         )
 
-    def test_removeColumns(self, bare_bones_data: DataModel):
+    def test_removeColumns(self, bare_bones_data: QDataModel):
         retvalue = bare_bones_data.removeColumns(1, 2)
         assert retvalue is True
         assert bare_bones_data._data.shape == (5, 1)
         assert bare_bones_data._data.columns == ["col0"]
 
-    def test_removeColumns_valid_parent(self, bare_bones_data: DataModel):
+    def test_removeColumns_valid_parent(self, bare_bones_data: QDataModel):
         """You can't remove columns inside cells."""
         assert (
             bare_bones_data.removeColumns(
