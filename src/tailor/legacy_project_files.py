5ad31a7f4c1cd9d9f05b1ae308ab7ee36a7d92fb
David Fokkema <davidfokkema@icloud.com>
2024-02-26 11:48:13 +0100

    Fix opening some legacy projects
    Projects without x_min or x_max, etc. Also: do not catch exceptions.

--- src/tailor/legacy_project_files.py
+++ src/tailor/legacy_project_files.py
@@ -8,24 +8,20 @@ DRAW_CURVE_OPTION_TABLE = [DrawCurve.ON_DATA, DrawCurve.ON_DOMAIN, DrawCurve.ON_
 
 
 def load_legacy_project(jsondict: dict) -> Project:
-    try:
-        sheet = get_sheet_from_project(jsondict)
-        plots = get_plots_from_project(jsondict)
-        project = Project(
-            application=jsondict["application"],
-            version=jsondict["version"],
-            sheet_num=2,
-            plot_num=len(plots) + 1,
-            sheets=[sheet],
-            plots=plots,
-            multiplots=[],
-            tab_order=["sheet_1"] + [f"plot_{plot.id}" for plot in plots],
-            current_tab=jsondict["current_tab"],
-        )
-        return project
-    except Exception as exc:
-        print(exc)
-        sys.exit()
+    sheet = get_sheet_from_project(jsondict)
+    plots = get_plots_from_project(jsondict)
+    project = Project(
+        application=jsondict["application"],
+        version=jsondict["version"],
+        sheet_num=2,
+        plot_num=len(plots) + 1,
+        sheets=[sheet],
+        plots=plots,
+        multiplots=[],
+        tab_order=["sheet_1"] + [f"plot_{plot.id}" for plot in plots],
+        current_tab=jsondict["current_tab"],
+    )
+    return project
 
 
 def get_sheet_from_project(jsondict: dict) -> Sheet:
@@ -65,10 +61,10 @@ def get_plots_from_project(jsondict: dict) -> list[Plot]:
                 y_err_col=tab["y_err_var"],
                 x_label=tab["xlabel"],
                 y_label=tab["ylabel"],
-                x_min=tab["xmin"],
-                x_max=tab["xmax"],
-                y_min=tab["ymin"],
-                y_max=tab["ymax"],
+                x_min=value if (value := tab["xmin"]) != "" else None,
+                x_max=value if (value := tab["xmax"]) != "" else None,
+                y_min=value if (value := tab["ymin"]) != "" else None,
+                y_max=value if (value := tab["ymax"]) != "" else None,
                 modelexpression=tab["model_func"],
                 parameters=parameters,
                 fit_domain=tab["fit_domain"],
