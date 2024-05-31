b58ea6e3e75ecb80d5f0333c596566bd47e0761e
David Fokkema <davidfokkema@icloud.com>
2024-05-31 16:27:06 +0200

    Preflight window with progress bar

--- src/tailor/__main__.py
+++ src/tailor/__main__.py
@@ -1,4 +1,7 @@
 import importlib
+import toga
+from toga.style import Pack, pack
+
 
 PREFLIGHT_MODULES = [
     "pydantic",
@@ -14,15 +17,40 @@ PREFLIGHT_MODULES = [
 ]
 
 
-def preflight_imports():
-    for module in PREFLIGHT_MODULES:
-        print(f"Importing {module}...")
-        importlib.import_module(module)
-    print("done.")
+class PreflightImports(toga.app.BackgroundTask):
+
+    def __call__(self, app, **kwargs):
+        app.progress.max = len(PREFLIGHT_MODULES)
+        for idx, module in enumerate(PREFLIGHT_MODULES):
+            app.progress.value = idx
+            app.status_label.text = f"Importing {module}..."
+            # update interface, yield a delay value?
+            yield 0.001
+            # import module
+            importlib.import_module(module)
+        app.loop.stop()
+
+
+class PreflightWindow(toga.App):
+    def startup(self) -> None:
+        main_box = toga.Box(style=Pack(direction=pack.COLUMN))
+        self.status_label = toga.Label("Importing ...")
+        self.progress = toga.ProgressBar()
+        main_box.add(self.status_label)
+        main_box.add(self.progress)
+
+        self.main_window = toga.MainWindow(title=self.formal_name)
+        self.main_window.content = main_box
+
+        # threading.Thread(target=self.preflight_imports).start()
+
+        self.main_window.show()
+        self.add_background_task(PreflightImports())
 
 
 if __name__ == "__main__":
-    preflight_imports()
+    PreflightWindow().main_loop()
+    print("done")
 
     from tailor.app import main
 
