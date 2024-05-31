6d9b0203a35b65c5655761238f8d977b311ecd29
David Fokkema <davidfokkema@icloud.com>
2024-05-31 15:05:36 +0200

    Preflight: import modules

--- src/tailor/__main__.py
+++ src/tailor/__main__.py
@@ -1,4 +1,29 @@
-from tailor.app import main
+import importlib
+
+PREFLIGHT_MODULES = [
+    "pydantic",
+    "numpy",
+    "scipy.optimize",
+    "scipy.stats",
+    "matplotlib.pyplot",
+    "pandas",
+    "lmfit",
+    "libcst",
+    "PySide6",
+    "pyqtgraph",
+]
+
+
+def preflight_imports():
+    for module in PREFLIGHT_MODULES:
+        print(f"Importing {module}...")
+        importlib.import_module(module)
+    print("done.")
+
 
 if __name__ == "__main__":
+    preflight_imports()
+
+    from tailor.app import main
+
     main()
