1dd64476ab4ddb60e5a02a5f06fda48c9604e2b5
David Fokkema <davidfokkema@icloud.com>
2024-09-13 15:10:23 +0200

    Open projects on cold start on macOS, fixes #84

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,20 @@
 # Changelog
 
+All notable changes to this project will be documented in this file.
+
+The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
+and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
+
+## [Unreleased]
+
+### Changed
+
+- Tailor now follows the [Keep a Changelog](https://keepachangelog.com/en/1.1.0/) conventions.
+
+### Fixed
+
+- Open Tailor projects from Finder, don't show an empty window (#84).
+
 ## 2.0.4
 
 ### :rocket: Features
--- src/tailor/__main__.py
+++ src/tailor/__main__.py
@@ -1,7 +1,7 @@
 import importlib
 import importlib.resources
 
-from PySide6 import QtGui, QtWidgets
+from PySide6 import QtCore, QtGui, QtWidgets
 
 PREFLIGHT_MODULES = [
     "pydantic",
@@ -17,33 +17,66 @@ PREFLIGHT_MODULES = [
 ]
 
 
-def preflight():
-    """Perform preflight.
+class PreflightApplication(QtWidgets.QApplication):
+    """Minimal application showing a splash screen.
 
-    This function imports modules listed in PREFLIGHT_MODULES. These include
+    This class imports modules listed in PREFLIGHT_MODULES. These include
     modules that are quite large and take some time to load, especially after a
     cold start when the OS preforms extra malware checks. While loading, a
     splash screen is shown to the user to indicate activity.
+
+    The application can also receive FileOpen events from macOS. If so, the
+    filename will be stored and can later be passed on to the main application.
+
     """
-    app = QtWidgets.QApplication()
-    pixmap = QtGui.QPixmap(
-        importlib.resources.files("tailor.resources") / "splashscreen.png"
-    )
-    splash = QtWidgets.QSplashScreen(pixmap)
-    splash.show()
-    for num, module in enumerate(PREFLIGHT_MODULES, start=1):
-        splash.showMessage(
-            f"Importing module {module}... {num}/{len(PREFLIGHT_MODULES)}"
+
+    file_to_open = None
+
+    def __init__(self) -> None:
+        super().__init__()
+
+        pixmap = QtGui.QPixmap(
+            importlib.resources.files("tailor.resources") / "splashscreen.png"
         )
-        app.processEvents()
-        importlib.import_module(module)
-    splash.close()
+        splash = QtWidgets.QSplashScreen(pixmap)
+        splash.show()
+        for num, module in enumerate(PREFLIGHT_MODULES, start=1):
+            splash.showMessage(
+                f"Importing module {module}... {num}/{len(PREFLIGHT_MODULES)}"
+            )
+            self.processEvents()
+            importlib.import_module(module)
+        splash.close()
+
+    def event(self, event):
+        """Handle system events."""
+        if event.type() == QtCore.QEvent.FileOpen:
+            # file open event from macOS, store filename for later retrieval
+            self.file_to_open = event.file()
+            return True
+        return super().event(event)
+
+
+def preflight():
+    """Perform preflight.
+
+    Returns:
+        str | None: filename if macOS requested to open a file.
+    """
+    app = PreflightApplication()
     app.shutdown()
+    return app.file_to_open
 
 
 if __name__ == "__main__":
-    preflight()
+    if file_to_open := preflight():
+        # macOS requested to open a file
+        import sys
+
+        # append filename to command-line arguments
+        sys.argv.append(file_to_open)
 
     from tailor.app import main
 
+    # run the main application
     main()
